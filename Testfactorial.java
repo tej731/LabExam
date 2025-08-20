1901ba0055f9e03ace14733ab603d57c9a73c28a
tej731 <160586019+tej731@users.noreply.github.com>
2025-08-20 10:32:22 +0530

    Update Testfactorial.java

--- Testfactorial.java
+++ Testfactorial.java
@@ -1,12 +1,14 @@
-public class Testfactorial{
-  public static void main(String[] args){
-    int res=factorial.findFactorial(5);
-    if(res!=120){
-      System.out.println("test failed");
+public class Testfactorial {
+    public static void main(String[] args) {
+        try {
+            int result = factorial.findFactorial(5);
+            if(result == 120) {
+                System.out.println("Test passed ");
+            } else {
+                System.out.println("Test failed ");
+            }
+        } catch(Exception e) {
+            System.out.println("Test error: " + e.getMessage());
+        }
     }
-    if(factorial.findFactorial(0)!=1){
-      System.out.println("test failed");
-    }
-    System.out.println("Successfully test cases passeed");
-  }
 }
